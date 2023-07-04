430cc324c9d8374c032179302fb45b381d55381f
Manuel Sandi <manuelsandi97@hotmail.com>
2023-07-03 19:14:15 -0600

    First commit

---
+++ src/blackjack/usecases/index.js
@@ -0,0 +1,6 @@
+
+export { crearDeck } from './create-deck';
+export { pedirCarta } from './pedir-carta';
+export { valorCarta } from './valor-cartas';
+export {turnoComputadora} from './turno-computadora';
+export {crearCartaHTML} from './crear-carta-html';
